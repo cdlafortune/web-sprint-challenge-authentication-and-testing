354e36912425ed2fea4d1839f4c8a819ff0b6bee
cdsuddreth <cdsuddreth@gmail.com>
2020-09-11 21:18:25 -0400

    endpoints complete, writing tests

--- api/server.js
+++ api/server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-
-const authenticate = require('../auth/authenticate-middleware.js');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const {authenticate} = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
-
+const db = require('../database/dbConfig');
+const secret = process.env.SECRET || "secret";
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: secret,
+    store: new KnexSessionStore({
+        knex: db,
+        createtable: true
+    })
+}));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,73 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const users = require('./auth-model.js');
+const {authenticate, validateUser} = require('./authenticate-middleware');
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.get('/users', authenticate, async (req, res, next) => {
+  try{
+    res.json(await users.find());
+  } catch(err){
+    next(err);
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/register', validateUser, async (req, res, next) => {
+  try {
+    const {username, password} = req.body;
+    const user = await users.findBy({username}).first();
+
+    if(user){
+      return res.status(409).json({message: "Username already exists."});
+    }
+
+    const newUser = await users.add({
+      username, 
+      password: await bcrypt.hash(password, 16)
+    });
+
+    res.status(201).json(newUser);
+
+  } catch(err){
+    next(err);
+  }
 });
+
+router.post('/login', validateUser, async (req, res, next) => {
+  try {
+    const {username, password} = req.body;
+    const user = await users.findBy({username}).first();
+
+    if(!user){
+      return res.status(401).json({message: "Invalid credentials."});
+    }
+
+    const validatePassword = await bcrypt.compare(password, user.password);
+
+    if (!validatePassword) {
+      return res.status(401).json({message: "Invalid credentials."});
+    }
+
+    req.session.user = user;
+
+    res.status(200).json(`Welcome, ${user.username}!`);
+
+  } catch(err){
+    next(err);
+  }
+});
+
+router.get("/logout", authenticate, async (req, res, next) => {
+	try {
+		req.session.destroy((err) => {
+			if (err) {
+				next(err)
+			} else {
+				res.status(204).end()
+			}
+		})
+	} catch (err) {
+		next(err)
+	}
+})
 
 module.exports = router;
